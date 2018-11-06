fb65da3a146c96b8982d7acf93520380cb2b0d06
hanshaw <hshaw@alumni.unc.edu>
2018-11-05 22:46:32 -0500

    almost finished with edits and troubleshooting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,29 @@ var lettersGuessed = [];
 var totalGuesses = 10;
 var guessesLeft = 0;
 var correctLetters = [];
+var wins = 0;
+var totalNumberWins = 0;
+var losses = 0;
 var blanks = 0;
 var lettersInWord = [];
 var blanksAndLetters = [];
+
+
+function reset() {
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    lettersInWord = selectedWord.split('');
+    blanks = lettersInWord.length;
+
+    letterGuessed = 0;
+    guessesLeft = 10;
+    lettersGuessed = [];
+    correctLetters = [];
+    totalNumberWins = 0;
+    blanksAndLetters = [];
+    alphabetList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    test = false;
+    startGame();
+}
 
 
 function startGame() {
@@ -17,6 +37,7 @@ function startGame() {
     blanks = lettersInWord.length;
 
     correctLetters = 0;
+    totalNumberWins = 0;
     guessesLeft = 10;
     lettersGuessed = [];
     blanksAndLetters = [];
@@ -24,12 +45,13 @@ function startGame() {
 
     for (var i = 0; i < blanks; i++) {
         blanksAndLetters[i] = "_";
-        document.querySelector("#guessWord").innerText = blanksAndLetters;
+        document.querySelector("#guess-word").innerText = blanksAndLetters;
     }
 
-    document.querySelector("#guessWord").innerText = blanksAndLetters.join(' ');
+    document.querySelector("#guess-word").innerText = blanksAndLetters.join(' ');
     document.querySelector("#guesses-left").innerText = guessesLeft;
-    document.querySelector('#letters-guessed').innerText = lettersGuessed;
+    document.querySelector("#letters-guessed").innerText = lettersGuessed;
+    document.querySelector("#total-losses").innerText = losses;
 
     console.log(selectedWord);
     console.log(lettersInWord);
@@ -37,55 +59,63 @@ function startGame() {
     console.log(blanksAndLetters);
 }
 
-function findGuess(letter) {
-    selectedWord = words[Math.floor(Math.random() * words.length)];
-    for (var i = 0; i < selectedWord.length; i++) {
-        var wordLetter = selectedWord[i];
-        if (wordLetter === letter) {
-            blanks[i] === letter;
-        }
-    }
-}
-
 function checkGuess(guess) {
     selectedWord = words[Math.floor(Math.random() * words.length)];
     if (selectedWord.indexOf(guess) > -1) {
-        if (lettersInWord[i] === guess) {
-            correctLetters++;
-            blanksAndLetters[i] = guess;
+        for (var i = 0; i < blanks; i++) {
+            if (lettersInWord[i] === guess) {
+                totalNumberWins++;
+                blanksAndLetters[i] = guess;
 
-            document.querySelector("#guessWord").innerText = blanksAndLetters.join(' ');
+                document.querySelector("#guess-word").innerText = blanksAndLetters.join(' ');
+            }
         }
-        else {
-            lettersGuessed.push(guess);
-            guessesLeft--;
 
-            document.querySelector("#guesses-left").innerText = guessesLeft;
-            document.querySelector("#letters-guessed").innerText = lettersGuessed;
+        console.log(blanksAndLetters);
+    }
+    else {
+        lettersGuessed.push(guess);
+        guessesLeft--;
 
-            console.log("Letters guessed - " + lettersGuessed);
-            console.log("Guesses left - " + guessesLeft);
-        }
+        document.querySelector("#guesses-left").innerText = guessesLeft;
+        document.querySelector("#letters-guessed").innerText = lettersGuessed;
+
+        console.log("Letters guessed - " + lettersGuessed);
+        console.log("Guesses left - " + guessesLeft);
+    }
+
+}
+
+
+function winsLosses(){
+    if(totalNumberWins === blanks){
+        wins++;
+
+        document.querySelector("#wins-number").innerText = wins;
+        reset();
+    }
+    else if(guessesRemaining === 0){
+        losses++;
+
+        document.querySelector("#total-losses").innerText = losses;
+        reset();
     }
 }
+
 
 startGame();
 
 document.onkeyup = function (event) {
-    guess = event.key;
-    findGuess(guess);
-};
+    test = true;
+    var letterGuessed = event.key;
+    for (var i = 0; i < alphabetList.length; i++) {
+        if (letterGuessed === alphabetList[i] && test === true) {
+            var breakAlphabetList = alphabetList.splice(i, 1);
 
-function reset(){
-    selectedWord = words[Math.floor(Math.random() * words.length)];
-    lettersInWord = selectedWord.split('');     
-    blanks = lettersInWord.length;
+            console.log("spliced letter is " + breakAlphabetList);
 
-    letterGuessed = 0;
-    guessesLeft = 10;
-    lettersGuessed = [];
-    blanksAndLetters = [];
-    alphabetList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    test=false;
-    startGame();
+            checkGuess(letterGuessed);
+            winsLosses();
+        }
+    }
 }
